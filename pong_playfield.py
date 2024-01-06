f0a025b63edb8f1b1f5f1caf8efa621f5e856209
JakubTrojan89 <kubatrojan89@gmail.com>
2024-01-06 17:59:07 +0100

    creating palette1

--- pong_playfield.py
+++ pong_playfield.py
@@ -18,6 +18,18 @@ game_window = pygame.display.set_mode((Playfield_width, Playfield_height), 0, 32
 # game_window title
 pygame.display.set_caption('Simple Pong')
 
+# in-game palette
+Palette_width = 100
+Palette_height = 20
+BLUE = (0, 0, 255)
+Palette_1_position = (350, 360) # palette primary position written in tuple
+# creating the palette and filling it in with color
+Palette1 = pygame.Surface((Palette_width, Palette_height))
+Palette1.fill(BLUE)
+# setting the square with the palette in a primary position
+Palette1_rectangle = Palette1.get_rect()
+Palette1_rectangle.x = Palette_1_position[0]
+Palette1_rectangle.y = Palette_1_position[1]
 # main loop
 while True:    # handling player-generated events
     for event in pygame.event.get():  # capture window close
@@ -27,6 +39,9 @@ while True:    # handling player-generated events
 
     # drawing objects
     game_window.fill(LT_BLUE) # window color
+
+    # draw the palette inside the game window
+    game_window.blit(Palette1, Palette1_rectangle)
 
     # update game window and display it
     pygame.display.update()
